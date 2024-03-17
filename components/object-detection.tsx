fc1e30ed366e78c8b3dec1f7979444cc2a276c02
abhisekdubey <102611596+abhisekdubey@users.noreply.github.com>
2024-03-17 23:33:37 +0530

    Ref : Objet detection done and some error resolved

--- components/object-detection.tsx
+++ components/object-detection.tsx
@@ -1,8 +1,10 @@
 "use client"
 import React, { useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
-import { load as cocossdload } from '@tensorflow-models/coco-ssd'
+import { load as cocoSSDLoad } from '@tensorflow-models/coco-ssd'
 import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-backend-webgl';
+import { renderPredictions } from "@/utils/render-predictions";
 
 let detectInterval
 
@@ -10,20 +12,50 @@ const ObjectDetection = () => {
 
     const [isLoading, setLoading] = useState(true)
     const webcamRef = useRef(null)
+    const canvasRef = useRef(null)
 
     const runCoco = async () => {
         setLoading(true)
-        const net = await cocossdload
+        // Before loading the model
+        tf.setBackend('webgl');
+        const net = await cocoSSDLoad()
         setLoading(false)
 
         // detect object in 10 mili second
         detectInterval = setInterval(() => {
-            // runObjectDetection(net)
+            runObjectDetection(net)
         }, 10)
     }
 
+    const runObjectDetection = async (net) => {
+        if (
+            canvasRef.current !== null &&
+            webcamRef.current !== null &&
+            webcamRef.current.video?.readyState === 4
+        ) {
+            canvasRef.current.width = webcamRef.current.video.videoWidth;
+            canvasRef.current.height = webcamRef.current.video.videoHeight;
+
+            // find detected objects
+            const detectedObjects = await net.detect(
+                webcamRef.current.video,
+                undefined,
+                0.6)
+
+            // net.detect(first_arg: what we want to show, second_arg: number of object detect, third_arg: accuracy )
+
+            const context = canvasRef.current.getContext('2d')
+            renderPredictions(detectedObjects, context)
+            // (first: data, second: context where render)
+
+        }
+    }
+
     const showMyVideo = () => {
-        if (webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
+        if (
+            webcamRef.current !== null &&
+            webcamRef.current.video?.readyState === 4
+        ) {
             const myVideoWidth = webcamRef.current.video.videoWidth;
             const myVideoHeight = webcamRef.current.video.videoHeight;
 
@@ -45,13 +77,16 @@ const ObjectDetection = () => {
                 )
                     :
                     (
-                        <div className=''>
+                        <div className='relative flex justify-center items-center gradient p-1.5 rounded-md'>
                             {/* WebCam */}
                             <Webcam
                                 ref={webcamRef}
                                 className='rounded-md w-full lg:h-[720px]'
                                 muted
                             />
+
+                            {/* canvas */}
+                            <canvas ref={canvasRef} className='absolute top-0 left-0 z-99999 w-full lg:h-[720px]' />
                         </div>
                     )
             }
