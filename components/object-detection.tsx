5e14699256883f2eec7957d22a50e33827e0f029
abhisekdubey <102611596+abhisekdubey@users.noreply.github.com>
2024-03-17 19:28:18 +0530

    Ref : setup AI coco ssd Model using tensor flow

--- components/object-detection.tsx
+++ components/object-detection.tsx
@@ -1,10 +1,26 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
+import { load as cocossdload } from '@tensorflow-models/coco-ssd'
+import * as tf from '@tensorflow/tfjs';
+
+let detectInterval
 
 const ObjectDetection = () => {
 
+    const [isLoading, setLoading] = useState(true)
     const webcamRef = useRef(null)
+
+    const runCoco = async () => {
+        setLoading(true)
+        const net = await cocossdload
+        setLoading(false)
+
+        // detect object in 10 mili second
+        detectInterval = setInterval(() => {
+            // runObjectDetection(net)
+        }, 10)
+    }
 
     const showMyVideo = () => {
         if (webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
@@ -17,21 +33,28 @@ const ObjectDetection = () => {
     }
 
     useEffect(() => {
+        runCoco()
         showMyVideo()
     }, [])
 
     return (
         <div className='mt-8'>
-            <div className=''>
-                {/* WebCam */}
-                <Webcam
-                    ref={webcamRef}
-                    className='rounded-md w-full lg:h-[720px]'
-                    muted
-                />
-
-                {/* Canvas */}
-            </div>
+            {
+                isLoading ? (
+                    <div className='gradient-title'>Loading AI Model</div>
+                )
+                    :
+                    (
+                        <div className=''>
+                            {/* WebCam */}
+                            <Webcam
+                                ref={webcamRef}
+                                className='rounded-md w-full lg:h-[720px]'
+                                muted
+                            />
+                        </div>
+                    )
+            }
         </div>
     )
 }
